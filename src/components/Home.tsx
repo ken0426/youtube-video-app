bcd0f027de39a489f5431454161c385cecebd416
ken0426 <kenkaitixyou0426@gmail.com>
2022-11-20 18:00:40 +0900

    [feat] 動画検索を行っている間のローディングアニメーションを追加

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,16 +1,29 @@
 import { useState } from 'react';
 import SearchArea from './SearchArea';
 import VideoArea from './VideoArea';
+import ReactLoading from 'react-loading';
+import './home.css';
 
 const Home = () => {
+  const [isLoading, setIsLoading] = useState(false);
   const [videoData, setVideoData] = useState([]);
   return (
-    <>
+    <div className='main'>
       <div className='header _mainBackGroundColorBlack'>
-        <SearchArea setVideoData={setVideoData} />
+        <SearchArea setVideoData={setVideoData} setIsLoading={setIsLoading} />
       </div>
-      <VideoArea videoData={videoData} />
-    </>
+      {!isLoading ? (
+        <VideoArea videoData={videoData} />
+      ) : (
+        <ReactLoading
+          type={'spin'}
+          color={'#ffffff'}
+          height={'10%'}
+          width={'10%'}
+          className={'loading'}
+        />
+      )}
+    </div>
   );
 };
 
--- src/components/SearchArea.tsx
+++ src/components/SearchArea.tsx
@@ -6,34 +6,40 @@ import './searchArea.css';
 
 interface Props {
   setVideoData: (e: []) => void;
+  setIsLoading: (e: boolean) => void;
 }
 
-const SearchArea = ({ setVideoData }: Props) => {
-  const [text, setText] = useState('UCFBkdFQ3iYvh882EjPnreYw');
+const SearchArea = ({ setVideoData, setIsLoading }: Props) => {
+  const [text, setText] = useState('UC17EHY8PqxX4lUeL5sbTjBQ');
 
   const onPressSearch = async () => {
+    setIsLoading(true);
     if (text.trim() === '') {
       alert('テキストを入力してください');
+      setIsLoading(false);
     } else {
       const resData = await callApi(text);
       const videoData = await videoList(resData);
       setVideoData(videoData);
+      setIsLoading(false);
     }
   };
 
   return (
-    <div className='searchArea'>
-      <input
-        type='search'
-        value={text}
-        placeholder={'検索'}
-        className='search _searchText _mainBackGroundColorBlack'
-        onChange={(e) => setText(e.target.value)}
-      />
-      <button className='searchButton _searchText' onClick={onPressSearch}>
-        <img className='searchIcon' src={searchIcon} alt={'虫眼鏡の画像'} />
-      </button>
-    </div>
+    <>
+      <div className='searchArea'>
+        <input
+          type='search'
+          value={text}
+          placeholder={'チャンネルID'}
+          className='search _searchText _mainBackGroundColorBlack'
+          onChange={(e) => setText(e.target.value)}
+        />
+        <button className='searchButton _searchText' onClick={onPressSearch}>
+          <img className='searchIcon' src={searchIcon} alt={'虫眼鏡の画像'} />
+        </button>
+      </div>
+    </>
   );
 };
 
